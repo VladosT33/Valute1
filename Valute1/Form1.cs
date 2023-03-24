507995763146935a56dee7ad9f24b37b86d23ab3
BEST <BEST@DESKTOP-5HRJJEK>
2023-03-24 12:07:12 +0200

    Добавьте файлы проекта.

---
+++ Valute1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Valute1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "=>";
+            textBox3.Enabled = false;
+            textBox4.Enabled = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "<=";
+            textBox3.Enabled = true;
+            textBox4.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double kurs1, kurs2, suma, sum_out;
+            kurs1 = double.Parse(textBox1.Text);
+            kurs2 = double.Parse(textBox2.Text);
+
+            if (radioButton1.Checked == true)
+            {
+                textBox4.Enabled = true;
+                suma = double.Parse(textBox3.Text);
+                sum_out = suma * kurs1;
+                textBox4.Text = sum_out.ToString();
+            }
+            else
+            {
+                textBox3.Enabled = true;
+                suma = double.Parse(textBox4.Text);
+                sum_out = suma / kurs1;
+                textBox3.Text = sum_out.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kurs1, kurs2, suma, sum_out;
+            kurs1 = double.Parse(textBox1.Text);
+            kurs2 = double.Parse(textBox2.Text);
+
+            if (radioButton1.Checked == true)
+            {
+            
+                    textBox4.Enabled = true;
+                    suma = double.Parse(textBox3.Text);
+                    sum_out = suma * kurs1;
+                    textBox4.Text = sum_out.ToString();
+                
+            }
+
+        }
+    }
+    }
+
